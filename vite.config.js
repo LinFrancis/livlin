a0c1502ab25cc0a61775b618966d1e02eeaa995a
Lin Francis <64030176+LinFrancis@users.noreply.github.com>
2025-08-18 01:07:22 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'vite'
-export default defineConfig({
-  base: '/',
-  build: { rollupOptions: { input: { main: 'index.html', servicios:'servicios.html', sobre:'sobre.html' } } }
-})
